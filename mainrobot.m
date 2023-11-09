4330aa3c93e3444215eddd02f154fba255fdd9a0
Fletcher Emmott <124654851+fletcheremmott@users.noreply.github.com>
2023-11-09 13:40:50 -0700

    mainrobot.m

--- mainrobot.m
+++ mainrobot.m
@@ -1,64 +1,69 @@
 %{
 touchfront = 1
-lightsensor = 3
-ultrasonic = 4
+lightsensor = 4
+ultrasonic = 2
 leftwheel = D
 rightwheeel = A
 lift = C
 %}
+rightTurn = -50;
+leftTurn = -50;
 motor = -40;
-threshold = 38;
+threshold = 50;
+distance = 0;
 
-
-brick.SetColorMode(3, 2);
+brick.SetColorMode(4, 2);
 
 while 1
     
     brick.MoveMotor('A', motor);
     brick.MoveMotor('D', motor);
 
-    %touch = brick.TouchPressed(1);
-    color = brick.ColorCode(3);
-    distance = brick.UltrasonicDist(4);
-    
+    touch = brick.TouchPressed(1);
+    color = brick.ColorCode(4);
+    distance = brick.UltrasonicDist(2);
     
     if color == 5                                         
-        disp('red');
         brick.StopMotor('AD', 'Brake');
         pause(5);
+        disp('red')
         brick.MoveMotor('A', motor); 
         brick.MoveMotor('D', motor);
         pause(0.5);
-    elseif color == 2 || color == 3 
-        disp('blue/green');
-        run('Keytest');
-        brick.MoveMotor('A', motor);
-        brick.MoveMotor('D', motor);
-        pause(6);
     end
     
-
-    if distance > threshold            
-        pause(0.60);
-        brick.StopMotor('AD');
-        brick.MoveMotor('D', 20);
-        pause(0.6);
-        brick.StopMotor('D');
+    if color == 2 || color == 3 
+        brick.StopMotor('AD', 'Brake');
+        run('Keytest.m');
+        disp('blue/green');
         brick.MoveMotor('A', motor); 
         brick.MoveMotor('D', motor);
         pause(2);
     end
-
-    %{
-    if touch == 1 
-        pause(1); 
-        disp('touched');
-        brick.StopMotor('AD');         
-        dist = brick.UltrasonicDist(4);
-        brick.MoveMotor('A', motor);
-        brick.MoveMotor('D', motor);
-        pause(3.5); 
+    
+    if distance > threshold            
+        pause(1.5);
         brick.StopMotor('AD', 'Brake');
+        brick.MoveMotor('A', rightTurn);
+        pause(0.6);
+        brick.StopMotor('A', 'Brake');
+        brick.MoveMotor('A', motor); 
+        brick.MoveMotor('D', motor);
+        pause(2.2);
     end
-    %}
+
+    if touch 
+        pause(0.4); 
+        disp('touched');
+        brick.StopMotor('AD');
+        brick.MoveMotor('A', -motor);
+        brick.MoveMotor('D', -motor);
+        pause(1);
+        brick.StopMotor('AD', 'Brake');
+        brick.MoveMotor('D', leftTurn);
+        pause(1);
+        brick.StopMotor('D', 'Brake');
+
+    end
+    
 end
