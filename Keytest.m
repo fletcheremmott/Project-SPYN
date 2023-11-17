dcb190e431411fe24d2d4283a433b5098316b1d9
Fletcher Emmott <124654851+fletcheremmott@users.noreply.github.com>
2023-11-16 17:06:30 -0700

    Keytest.m

--- Keytest.m
+++ Keytest.m
@@ -4,32 +4,53 @@ InitKeyboard();
 while 1
     pause(0.1);
     switch key
-        case 0
-            brick.StopAllMotors();
+        
         case 'uparrow'
             brick.MoveMotor('A', 40);
             brick.MoveMotor('D', 40);
+            pause(0.5)
+            brick.StopMotor('AD', 'Coast');
             
         case 'downarrow'
             brick.MoveMotor('A', -40);
             brick.MoveMotor('D', -40);
+            pause(.5)
+            brick.StopMotor('AD', 'Brake');
             
         case 'leftarrow'
             brick.MoveMotor('A', 15);
             brick.MoveMotor('D', -15);
+            pause(0.25)
+            brick.StopMotor('AD', 'Brake');
             
         case 'rightarrow'
             brick.MoveMotor('A', -15)
             brick.MoveMotor('D', 15);
+            pause(0.25)
+            brick.StopMotor('AD', 'Brake');
 
         case 'w'
-            brick.MoveMotor('C',-70)
+            brick.MoveMotor('C',-50)
+            pause(.15)
+            brick.StopMotor('C', 'Brake');
 
         case 's'
-            brick.MoveMotor('C',70)
+            brick.MoveMotor('C',50)
+            pause(.15)
+            brick.StopMotor('C', 'Brake');
 
         case 'q'
-            break    
+            break
+
+        case 'e'
+            brick.MoveMotor('B', 30)
+            pause(.15)
+            brick.StopMotor('B', 'Brake');
+
+        case 'd'
+            brick.MoveMotor('B', -30)
+            pause(.15)
+            brick.StopMotor('B', 'Brake');
     end
 end
 CloseKeyboard();
